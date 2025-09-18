f4fb8a05b06788f2cfd8923236ddce21043a00bd
ahmedmenged1234 <ahmdmnjd806@gmail.com>
2025-09-18 15:08:56 +0300

    edit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,11 +5,6 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: "file:./dev.db"
-    }
-  },
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
 })
 
